7c6b28086f3e8e20d5f7b9ca401ce8baaabc29c1
Dhanesh Vishwakarma <dhanesh.vishwakarma11@gmail.com>
2020-03-27 17:08:49 +0530

    server file added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const logger = require('morgan')
+const postRoutes = require('./routes/postRoutes')
+const getRoutes = require('./routes/getRoutes')
+
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+app.use(postRoutes)
+app.use(getRoutes)
+
+app.listen(port, () => console.log(`Server: listening... on PORT ${port}`))
